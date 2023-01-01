e329ebe8c6af0ae2fa2f714dd1ccd601a09d459f
Nicolas Sapountzis <nsap95@gmail.com>
2023-01-01 19:03:08 +0700

    commit tree f170d226e16588ec21f17555dfc9d960b3b51572

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const newArray = [...cats, "Broom"];
+    return newArray
+}
+
+function prependCat(name) {
+    const newArray = ["Arnold", ...cats];
+    return newArray
+}
+
+function removeLastCat(name) {
+    return cats.slice(0,2)
+}   
+
+function removeFirstCat(name) {
+    return cats.slice(1)
+}   
